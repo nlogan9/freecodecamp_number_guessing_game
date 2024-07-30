32280e5de9caa601e3043ef9cd507deb2abe5878
Nathan Logan <nathanlogan81@gmail.com>
2024-07-30 20:39:00 +0000

    feat: store name to database

--- number_guess.sh
+++ number_guess.sh
@@ -9,9 +9,15 @@ read name
 if [[ $($PSQL "SELECT name FROM users WHERE name = '$name'") ]]
 then
   # add lookup and output
+  ID=$($PSQL "SELECT user_id FROM users WHERE name = '$name'")
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(user_id) FROM games WHERE user_id = $ID")
+  MIN=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $ID")
+  echo "Welcome back, $name! You have played $GAMES_PLAYED games, and your best game took $MIN guesses."
   exit
 else
   echo "Welcome, $name! It looks like this is  your first time here."
+  INSERT=$($PSQL "INSERT INTO users(name) VALUES('$name')")
+  ID=$($PSQL "SELECT user_id FROM users WHERE name = '$name'")
 fi
 
 echo $RND
