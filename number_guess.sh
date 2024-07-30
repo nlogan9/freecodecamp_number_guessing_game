cae1a199a86367b53ac0c7f4a436618ae89efb78
Nathan Logan <nathanlogan81@gmail.com>
2024-07-30 20:24:54 +0000

    feat: add user name input

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 RND=$(expr 1 + $RANDOM % 1000)
+
+echo "Enter your username:"
+read name
+
+if [[ $($PSQL "SELECT name FROM users WHERE name = '$name'") ]]
+then
+  # add lookup and output
+  exit
+else
+  echo "Welcome, $name! It looks like this is  your first time here."
+fi
 
 echo $RND
