9b26c49c2d32a2a14bd84f017a755ddeefad4f46
Nathan Logan <nathanlogan81@gmail.com>
2024-07-30 21:07:50 +0000

    fix: pulled name from db for echo

--- number_guess.sh
+++ number_guess.sh
@@ -9,12 +9,13 @@ read name
 if [[ $($PSQL "SELECT name FROM users WHERE name = '$name'") ]]
 then
   # add lookup and output
+  NAME=$(echo $($PSQL "SELECT name FROM users WHERE name = '$name'") | sed -E 's/^ *| *$//g')
   ID=$($PSQL "SELECT user_id FROM users WHERE name = '$name'")
   GAMES_PLAYED=$($PSQL "SELECT COUNT(user_id) FROM games WHERE user_id = $ID")
   MIN=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $ID")
   echo "Welcome back, $name! You have played $GAMES_PLAYED games, and your best game took $MIN guesses."
 else
-  echo "Welcome, $name! It looks like this is  your first time here."
+  echo "Welcome, $NAME! It looks like this is  your first time here."
   INSERT=$($PSQL "INSERT INTO users(name) VALUES('$name')")
   ID=$($PSQL "SELECT user_id FROM users WHERE name = '$name'")
 fi
