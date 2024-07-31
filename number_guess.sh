d41946702ea355261515a5e4788f79a38752219c
Nathan Logan <nathanlogan81@gmail.com>
2024-07-31 13:53:02 +0000

    fix: formatting

--- number_guess.sh
+++ number_guess.sh
@@ -11,13 +11,15 @@ then
   # add lookup and output
   NAME=$(echo $($PSQL "SELECT name FROM users WHERE name = '$name'") | sed -E 's/^ *| *$//g')
   ID=$($PSQL "SELECT user_id FROM users WHERE name = '$name'")
+  echo $ID
   GAMES_PLAYED=$($PSQL "SELECT COUNT(user_id) FROM games WHERE user_id = $ID")
   MIN=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id = $ID")
-  echo "Welcome back, $name! You have played $GAMES_PLAYED games, and your best game took $MIN guesses."
+  echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $MIN guesses."
 else
   echo "Welcome, $name! It looks like this is  your first time here."
   INSERT=$($PSQL "INSERT INTO users(name) VALUES('$name')")
   ID=$($PSQL "SELECT user_id FROM users WHERE name = '$name'")
+  echo $ID
 fi
 
 TRIES=0
